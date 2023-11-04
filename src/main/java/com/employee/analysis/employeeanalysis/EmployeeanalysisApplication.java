682fc17bf068b68fcad6e160f9071cd84d2a81c0
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-11-04 10:52:21 +0000

    employee analysis springboot api

---
+++ src/main/java/com/employee/analysis/employeeanalysis/EmployeeanalysisApplication.java
@@ -0,0 +1,13 @@
+package com.employee.analysis.employeeanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeanalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeanalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee/analysis/employeeanalysis/EmployeeanalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.analysis.employeeanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeanalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
