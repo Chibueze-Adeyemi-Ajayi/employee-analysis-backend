35e49c424d404098dd9c95c8b8c5c8f888de3659
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-11-04 11:32:12 +0000

    employee analysis springboot api

--- src/main/java/com/employee/analysis/employeeanalysis/User/UserService.java
+++ src/main/java/com/employee/analysis/employeeanalysis/User/UserService.java
@@ -3,7 +3,9 @@ package com.employee.analysis.employeeanalysis.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserService implements UserDetailsService {
 
     @Override
